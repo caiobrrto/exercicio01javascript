98ad2eb9ef3956b330306eb766f30388c6a775cf
Caio Dos Santos Barreto <09041750576@info.local>
2023-07-17 19:07:14 -0300

    first commit

---
+++ Exercicios.js
@@ -0,0 +1,84 @@
+var prompt = require('prompt-sync')();
+
+
+// ex01
+
+// let nome = parseInt(prompt("Digite seu nome: "))
+// let cargo = parseInt(prompt("Digite seu salario: "))
+// let salario = parseInt(prompt("Digite seu salario: "))
+
+// if (salario < 1000){
+// }console.log("Olá" + nome + "Seu salário com acrescimo é: " + (salario + salario * 0.1) );
+
+
+// ex02
+
+// let salario = parseInt(prompt("Digite seu sálario, para ver se você tem direito ao aumento: "))
+
+// if (salario < 500){
+//  console.log( "Seu salário com aumento é: " + (salario + salario * 0.3));
+
+// }else (salario > 500){
+// console.log("Lamentamos, você não tem direito a aumento no momento!");
+// }
+
+
+// ex03
+
+// let numero1 = parseInt(prompt("Digite um numero: "))
+
+// if (numero < 0)
+//     console.log("Seu numero é positivo.");
+// else if(numero < 0)
+// console.log("Seu numero é negativo.");
+// else
+// console.log("0")
+
+
+// ex04
+
+    // let numero = parseInt(prompt("Digite um número:"));
+
+    // if (numero % 2 === 0) {
+    // console.log("O número digitado é par.");
+    // } else {
+    // console.log("O número digitado é ímpar.");
+    // }
+
+// ex05
+
+// let valor1 = parseFloat(prompt("Digite o primeiro valor"));
+// let valor2 = parseFloat(prompt("Digite o segundo valor"));
+
+if(valor1 > valor2){
+console.log("O primeiro valor é maior: " + valor1)
+
+}else if (valor2 > valor1){
+console.log("O segundo valor é maior: " + valor2)
+
+}else{
+    console.log("Os valores são iguais!")
+}
+
+
+
+// ex06
+
+// nota1 + parseInt(prompt)("Qual a primeira nota")
+// nota2 + parseInt(prompt)("Qual a segunda nota")
+// nota3 + parseInt(prompt)("Qual a terceira nota")
+// nota4 + parseInt(prompt)("Qual a quarta nota")
+
+// let media = (nota1 + nota2 + nota3 + nota4) /4
+
+// console.log("A media foi" + media)
+
+// if(media > 7){
+//     console.log("O aluno foi aprovado")
+
+// }else if(media < 5){
+//     console.log("O aluno foi reprovado")
+
+// }else{
+// console.log("O aluno está em recuperação")
+// }
---
+++ exercicios02.js
@@ -0,0 +1,85 @@
+//let prompt = require('prompt-sync')()
+
+// ex01
+
+// let nomeUsuario = prompt("Digite o nome de usuario: ");
+// let senha = prompt("Digite a senha: ");
+
+// if (nomeUsuario == "admin" && senha == "senha123"){
+//     console.log("Login bem sucedido!")
+// }else{
+//     console.log("Nome de usuario ou senha incorretos")
+// }
+
+
+//ex02
+
+// let prompt = require('prompt-sync')()
+
+// let numero = parseInt(prompt("Digite um numero de 1 a 7: "))
+
+// switch (numero) {
+//     case 1:
+// console.log ("Segunda-feira")
+//     break;
+//     case 2:
+//         console.log ("Terça-feira")
+//         break;
+//         case 3:
+//             console.log ("Quarta-feira")
+//             break;
+//             case 4:
+//             console.log ("Quinta-feira")
+//             break;
+//             case 5:
+//             console.log ("Sexta-feira")
+//             break;
+//             case 6:
+//             console.log ("Sabado")
+//             break;
+//             case 7:
+//             console.log ("Domingo")
+//             break;
+
+//     default:
+//         console.log ("Numero invalido")
+// break;
+// }
+
+
+// ex03
+
+// let prompt = require('prompt-sync')()
+
+// let numero1 = parseFloat(prompt("Digite o primeiro número:"));
+// let numero2 = parseFloat(prompt("Digite o segundo número:"));
+// let numero3 = parseFloat(prompt("Digite o terceiro número:"));
+
+
+// if (numero1 < numero2 && numero2 < numero3) {
+//   console.log("Os números estão em ordem crescente.");
+// } else {
+//   console.log("Os números não estão em ordem crescente.");
+// }
+
+// ex04
+
+// let prompt = require('prompt-sync')()
+
+// let anoNasc = parseFloat(prompt("Digite seu ano de nascimento:"));
+
+// if (anoNasc < 2004){
+//     console.log("Maior de idade.")
+// } 
+//     else{
+//     (anoNasc > 2004)
+//     console.log("Menor de idade.")
+// }
+
+
+// ex05
+
+let prompt = require('prompt-sync')()
+
+let valorTotal = parseFloat(prompt("Digite o valor total da compra:"));
+
